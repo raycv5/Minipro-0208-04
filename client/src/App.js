782be2bf95f1fa7937e64a9cbe21b0dde1367b63
raycv5 <rayhanaw612@gmail.com>
2023-10-12 16:50:50 +0700

    Add path to organizer register page

--- client/src/App.js
+++ client/src/App.js
@@ -4,30 +4,35 @@ import { setData } from "./redux/event";
 import axios from "axios";
 import { useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
-import { EventPage } from "./pages/EventPage";
+import EventPage from "./pages/EventPage";
+import { OrganizerRegisterPage } from "./pages/OrganizerRegisterPage";
 function App() {
-   const dispatch = useDispatch();
-   const id = localStorage.getItem("id");
+  const dispatch = useDispatch();
+  const id = localStorage.getItem("id");
 
-   const keepLogin = async () => {
-      try {
-         const response = await axios.get(`http://localhost:2000/events`);
-         dispatch(setData(response.data));
-         console.log(response.data);
-      } catch (err) {
-         console.log(err);
-      }
-   };
-   useEffect(() => {
-      keepLogin();
-   }, []);
+  const keepLogin = async () => {
+    try {
+      const response = await axios.get(`http://localhost:2000/events`);
+      dispatch(setData(response.data));
+      console.log(response.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  useEffect(() => {
+    keepLogin();
+  }, []);
 
-   return (
-      <Routes>
-         <Route path="/" element={<HomePage />}></Route>
-         <Route path="/joinwithus" element={<EventPage />}></Route>
-      </Routes>
-   );
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />}></Route>
+      <Route path="/joinwithus" element={<EventPage />}></Route>
+      <Route
+        path="/register/organizer"
+        element={<OrganizerRegisterPage />}
+      ></Route>
+    </Routes>
+  );
 }
 
 export default App;
