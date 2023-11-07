c97509a2cb752cda27549b0c28320e2b76dfca04
raycv5 <rayhanaw612@gmail.com>
2023-11-08 01:43:57 +0700

    Fix user dashboard

--- client/src/components/dashboard-component/SideBar.jsx
+++ client/src/components/dashboard-component/SideBar.jsx
@@ -1,56 +1,44 @@
-import { 
-  Avatar,
-  Flex,
-  Icon,
-  Text 
-} from "@chakra-ui/react";
-import { PiTicketFill } from "react-icons/pi"
-import { MdAttachEmail, MdEventNote } from "react-icons/md"
+import { Avatar, Flex, Icon, Text } from "@chakra-ui/react";
+import { PiTicketFill } from "react-icons/pi";
+import { MdAttachEmail, MdEventNote } from "react-icons/md";
 
 const Sidebar = () => {
-
   const flexStyle = {
     alignItems: "center",
-    gap : "10px",
+    gap: "10px",
     cursor: "pointer",
     p: "5px",
-    _hover: { bg : "blue.300", color: "white" }
-  }
+    _hover: { bg: "green.400", color: "white", borderRadius: "5px" },
+  };
 
-  return (  
-    
+  return (
     <>
-    <Flex 
-      flexDir={"column"} 
-      m={"10px 10px"} 
-      gap={"30px"}
-      fontSize={"20px"}
-      >
-     
-     <Text fontWeight={500} ml={"5px"} mt={"20px"}>DASHBOARD</Text> 
-     <Flex {...flexStyle}>
-      <Icon as={Avatar} boxSize={"20px"}/>
-      <Text>Welcome</Text>
-     </Flex>
-    
-     <Flex {...flexStyle}>
-      <Icon as={PiTicketFill} boxSize={"25px"}/>
-      <Text>Refferal Code</Text>
-     </Flex>
+      <Flex flexDir={"column"} m={"10px 10px"} gap={"30px"} fontSize={"20px"}>
+        <Text fontWeight={500} ml={"5px"} mt={"20px"}>
+          DASHBOARD
+        </Text>
+        <Flex {...flexStyle}>
+          <Icon as={Avatar} boxSize={"20px"} />
+          <Text>Welcome</Text>
+        </Flex>
 
-     <Flex {...flexStyle}>
-      <Icon as={MdAttachEmail} boxSize={"25px"}/>
-      <Text>Invite/Send</Text>
-     </Flex> 
+        <Flex {...flexStyle}>
+          <Icon as={PiTicketFill} boxSize={"25px"} />
+          <Text>Refferal Code</Text>
+        </Flex>
 
-     <Flex {...flexStyle}>
-      <Icon as={MdEventNote} boxSize={"25px"}/>
-      <Text>Your Event</Text>
-     </Flex>
-         
-    </Flex>
+        <Flex {...flexStyle}>
+          <Icon as={MdAttachEmail} boxSize={"25px"} />
+          <Text>Invite/Send</Text>
+        </Flex>
+
+        <Flex {...flexStyle}>
+          <Icon as={MdEventNote} boxSize={"25px"} />
+          <Text>Your Event</Text>
+        </Flex>
+      </Flex>
     </>
   );
-}
- 
+};
+
 export default Sidebar;
